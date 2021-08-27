740e722c06a899bd78308a0e98dae76a708e38b9
albertchitta <albert.w.chittaphong@gmail.com>
2021-08-26 21:45:38 -0500

    Created Fattest Bear section

---
+++ src/javascripts/helpers/data/fbBearData.js
@@ -0,0 +1,30 @@
+// API CALLS FOR AUTHORS
+import axios from 'axios';
+import firebaseConfig from '../../../api/apiKeys';
+
+const dbUrl = firebaseConfig.databaseURL;
+
+// GET BEARS
+const getAuthors = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/authors.json`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
+// DELETE BEAR
+// CREATE BEAR
+const createAuthor = (authorObj) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/authors.json`, authorObj)
+    .then((response) => {
+      const body = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/authors/${response.data.name}.json`, body)
+        .then(() => {
+          getAuthors().then(resolve);
+        });
+    }).catch((error) => reject(error));
+});
+
+// UPDATE BEAR
+// SEARCH BEARS
+
+export { getAuthors, createAuthor };
